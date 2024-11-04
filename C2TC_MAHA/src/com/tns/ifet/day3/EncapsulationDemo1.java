b32db26a513aef606864dc3dae9f850ad856481f
admin <admin@Maha-CM411T0>
2024-11-04 23:15:42 +0530

    Encapsulation

---
+++ C2TC_MAHA/src/com/tns/ifet/day3/EncapsulationDemo1.java
@@ -0,0 +1,13 @@
+package com.tns.ifet.day3;
+
+public class EncapsulationDemo1 {
+
+	public static void main(String[] args) {
+		OopsConceptDemo1 obj = new OopsConceptDemo1(); 
+				obj.setSerialNum(101); 
+				obj.setName("Pooja"); 
+				obj.setAge(20); 
+				System.out.println(obj); 
+	}
+
+}
